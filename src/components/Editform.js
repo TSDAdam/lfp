12061ed0a25c5181b4c18a6619c007a4a6453f96
Adam Richards <adam@adamr.co.uk>
2022-10-30 19:57:06 +0000

    stuck trying to update cards array

--- src/components/Editform.js
+++ src/components/Editform.js
@@ -3,6 +3,25 @@ function Editform({ cards, setCards, id }) {
   const editThisCard = thisCard.id === id; // trying to match id of passed card to correct card in 'cards' array.
   console.log(editThisCard);
 
+  function saveChanges(cardtochange) {
+    setCards(
+      cards.map(
+        (
+          card // intention is map back over the original array, and if the id matches that
+        ) =>
+          card.id === id // of the edited card, write the changed card back in at its ID
+            ? {
+                id: id,
+                gamename: cardtochange.gamename,
+                maxplayers: cardtochange.maxplayers,
+                freespaces: cardtochange.freespaces,
+                tablenum: cardtochange.tablenum,
+              }
+            : card // ... or just write the original back in place.
+      )
+    );
+  }
+
   return (
     <>
       {editThisCard && ( // should only render if editThisCard is true.
@@ -18,7 +37,11 @@ function Editform({ cards, setCards, id }) {
           <p>Table #: </p>
           <input type="text" value={thisCard.tablenum}></input>
           <p></p>
-          <button type="button" className="playbutton">
+          <button
+            type="button"
+            className="playbutton"
+            onClick={saveChanges(thisCard)} //remove to see edit form - leave in for perpetual loop.
+          >
             Save changes
           </button>
         </div>
